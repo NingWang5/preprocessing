fa10652806670b9969630c697b7ce93639b73e08
NingWang5 <wongning@mail.ustc.edu.cn>
2023-03-19 14:42:56 +0800

    modify

--- preprocessing.py
+++ preprocessing.py
@@ -7,18 +7,24 @@ import os
 percent = 60
 times = 10
 folder_path = '/root/wangning/data'
-label_path = '/root/wangning/label.csv'
-output_path = '/root/wangning/data.csv'
+label_path = '/root/wangning/preprocessing/label.csv'
+output_path = '/root/wangning/preprocessing/data.csv'
 #####################################################
 
 label = pd.read_csv(label_path)
 files = os.listdir(folder_path)
-ls = []
 
-for file in files:
+files2 = [i for i in files if 'xr' in i]
+files1 = [i for i in files if i not in files2]
+files1 = sorted(files1)
+files2 = sorted(files2)
 
-    ls.append(pd.DataFrame(columns=['file', 'x', 'y', 'z', 'a']))
-    data = ls[-1]
+ls1 = []
+
+for file in files1:
+
+    ls1.append(pd.DataFrame(columns=['file', 'x', 'y', 'z', 'a']))
+    data = ls1[-1]
 
     data = pd.read_table(os.path.join(folder_path, file), header=None, skiprows=1)
     data = data.applymap(lambda x: float(str(x).replace(',', '.')))
@@ -59,18 +65,82 @@ for file in files:
             break
 
     if t == -1:
-        print(file)
+        print(file, "is not found")
         continue
         # raise "Not Find Time"
     
+    start = ls1[-2].iloc[-1].at['label'] if len(ls1) > 1 else 0
+
     length = len(data)
-    labels = np.linspace(t, 0, length)
+    interval = (t - start) / length if len(ls1) > 1 else 0
+    labels = np.linspace(start+interval, t, length)
     data['label'] = labels
 
-    ls[-1] = data
+    ls1[-1] = data
     print(data)
+
+
+ls2 = []
+
+for file in files2:
+
+    ls2.append(pd.DataFrame(columns=['file', 'x', 'y', 'z', 'a']))
+    data = ls2[-1]
+
+    data = pd.read_table(os.path.join(folder_path, file), header=None, skiprows=1)
+    data = data.applymap(lambda x: float(str(x).replace(',', '.')))
+    data.columns = ['file', 'x', 'y', 'z', 'a', 'label']
+    data['file'] = file
     
- 
+    temp = data[['x', 'y', 'z']].applymap(lambda x: abs(x))
+    x_mean = temp['x'].mean() * percent / 100
+    y_mean = temp['y'].mean() * percent / 100
+    z_mean = temp['z'].mean() * percent / 100
+
+    drop_list = []
+    for idx, row in data.iterrows():
+        if abs(row['x']) > x_mean:
+            break
+        drop_list.append(idx)
+    data = data.drop(index=drop_list)
+    data = data.reset_index(drop=True)
+
+    num = 0
+    for idx, row in data.iterrows():
+        if abs(row['x']) < x_mean:
+            num += 1
+        else:
+            num = 0
+        if num > times:
+            num = idx - times
+            break
+    length = len(data)
+    num = length if num == 0 else num
+    data = data.drop(index=list(range(num, length)))
+    data = data.reset_index(drop=True)
+
+    t = -1
+    for idx, row in label.iterrows():
+        if row['name'] in file and row['category'] == file[0]:
+            t = row['time']
+            break
+
+    if t == -1:
+        print(file, "is not found")
+        continue
+        # raise "Not Find Time"
+    
+    start = ls2[-2].iloc[-1].at['label'] if len(ls2) > 1 else 0
+
+    length = len(data)
+    interval = (t - start) / length if len(ls2) > 1 else 0
+    labels = np.linspace(start+interval, t, length)
+    data['label'] = labels
+
+    ls2[-1] = data
+    print(data)
+
+ls = ls1 + ls2
 data = pd.concat(ls, ignore_index=True)
 print(data)
 data.to_csv(output_path, index=None)
